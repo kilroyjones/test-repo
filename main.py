8c7993892a0a03c26ac42187bd5e58de3eab9f4c
kilroy <tsbach@gmail.com>
2025-11-27 17:57:25 +0800

    Test on prod

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+
+print('hi')
+print("again")
+print("again again")
+print("again again")
+print("yay")
+print("yay2")
+print("staging works?"
+print("staging works?"
+print("staging works?"
+print("staging works?"
+print("staging works?"
+print("staging works?"
+
 print('hi')
 print("again")
 print("again again")
